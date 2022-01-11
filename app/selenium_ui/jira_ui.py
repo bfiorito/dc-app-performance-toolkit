c332183e4e0c64fdf3fbadbd60b042b4e1241fc5
Bast <bastien.fiorito@valiantys.com>
2022-01-11 10:06:59 +0100

    specials tests for CSC

--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -65,5 +65,8 @@ Refer to `app/selenium_ui/jira/modules.py` for examples.
 
 
 # this action should be the last one
-def test_2_selenium_z_log_out(jira_webdriver, jira_datasets, jira_screen_shots):
-    modules.log_out(jira_webdriver, jira_datasets)
+def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
+     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+
+def test_2_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
+     extension_ui.app_specific_action2(jira_webdriver, jira_datasets)
